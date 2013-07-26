c4dbb4f396500d6cf866cb6db381eec537c5015d
Michal Špaček <tupinek@gmail.com>
2013-07-26 15:00:03 +0200

    Prepare for 0.02 version.

--- CZ.pm
+++ CZ.pm
@@ -15,7 +15,7 @@ Readonly::Array our @EXPORT_OK => qw(first_male first_female middle_female
 	last_male last_female middle_male middle_female name);
 
 # Version.
-our $VERSION = '0.01';
+our $VERSION = 0.02;
 
 # First and middle male names.
 our @first_male = our @middle_male = qw(
